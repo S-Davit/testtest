6b44a447a72856041d7c15c087a4f3142705a211
S-Davit <smbatyandavit6@gmail.com>
2023-10-02 20:51:56 +0400

    first commit

---
+++ meni.js
@@ -0,0 +1,44 @@
+
+
+// let x = 12545675431;
+// let y = 225465677562;
+
+
+//  y = y + x;
+//  x = y - x;
+//  y = y - x;
+
+//  console.log(y)
+
+//  console.log(x)
+//  /////////////////////////////////////////////////////////
+//  let x = "5";
+//  let y = 5;
+//  let z = "2";
+//  console.log(x+y-z)
+
+// let x = "5";
+// let y = "5";
+// let z = x+y;
+// console.log(typeof +z)
+/////////////////////////////////////////////////////////////
+
+// let x = "Davo"
+// alert(x)
+
+// let x = "Davo"
+// confirm(x)
+
+// let x = prompt ("tiv1")
+// let y = prompt ("tiv2")
+// alert(x +  y)
+
+
+
+
+
+
+let qash = 98;
+let boy = 1.8;
+let z = qash /(boy*boy)
+console .log(z)
